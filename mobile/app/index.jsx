3d71a0eb225235a8090e332c19586bb0911a981a
Hudson Andrade Viana <hudson1206@gmail.com>
2025-07-30 22:05:55 -0300

    Assets

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,19 +1,15 @@
-import { Link } from "expo-router";
-import { Image, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ color: "blue" }}>
-        Edit app/index.tsx to edit this screen.
-      </Text>
-      <Link href={"/about"}>Sobre</Link>
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'purple',
+  },
+})
