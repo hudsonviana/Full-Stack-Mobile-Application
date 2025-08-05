3cac6b24b17d8511f7790bdd535135de60947588
Hudson Andrade Viana <hudson1206@gmail.com>
2025-08-05 09:48:23 -0300

    Fetch transactions

---
+++ mobile/hooks/useTransactions.js
@@ -0,0 +1,73 @@
+import { useCallback, useState } from 'react'
+import { Alert } from 'react-native'
+
+// const API_URL = 'http://localhost:5001/api'
+const API_URL = 'http://192.168.1.70:5001/api'
+
+export function useTransactions(userId) {
+  const [transactions, setTransactions] = useState([])
+  const [summary, setSummary] = useState({
+    balance: 0,
+    income: 0,
+    expenses: 0,
+  })
+  const [isLoading, setIsLoading] = useState(true)
+
+  //
+  const fetchTransactions = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${userId}`)
+      const data = await response.json()
+      setTransactions(data)
+    } catch (error) {
+      console.error('Erro ao pegar as transações:', error)
+    }
+  }, [userId])
+
+  //
+  const fetchSummary = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/summary/${userId}`)
+      const data = await response.json()
+      setSummary(data)
+    } catch (error) {
+      console.error('Erro ao pegar o sumário:', error)
+    }
+  }, [userId])
+
+  //
+  const loadData = useCallback(async () => {
+    if (!userId) return
+
+    setIsLoading(true)
+
+    try {
+      await Promise.all([fetchTransactions(), fetchSummary()])
+    } catch (error) {
+      console.error('Erro ao carregar os dados:', error)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [fetchTransactions, fetchSummary, userId])
+
+  //
+  const deleteTransacion = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${id}`, {
+        method: 'DELETE',
+      })
+
+      if (!response.ok) {
+        throw new Error('Falha ao deletar transação')
+      }
+
+      loadData()
+      Alert.alert('Sucesso', 'Transação deletada com sucesso!')
+    } catch (error) {
+      console.error('Erro ao deletar transação:', error)
+      Alert.alert('Erro', error.message)
+    }
+  }
+
+  return { transactions, summary, isLoading, loadData, deleteTransacion }
+}
